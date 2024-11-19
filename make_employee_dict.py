1695940200562e40613e1ec74f944622a3c4a49d
PringleElUno <pringlel@oregonstate.edu>
2024-11-18 21:19:51 -0800

    Assignment 8c

---
+++ make_employee_dict.py
@@ -0,0 +1,85 @@
+# Author: Luis Pringle
+# GitHub username: PringleElUno
+# Date: 11/18/2024
+# Define a class named Employee that has private data members for an employees
+# _name, _ID_number, _salary, and _email address.
+
+# Define the class as employee
+class Employee:
+    """
+    Class: Employee that has _name, _ID_number, _salary, and _email address.
+
+    Attributes:
+    _name (str): The name of the employee.
+    _ID_number (str): The ID number of the employee.
+    _salary (float): The salary of the employee.
+    _email_address (str): the email address of the employee.
+
+    Methods:
+    get_name(): Returns the name of the employee.
+    get_ID_number(): Returns the ID number of the employee.
+    get_salary(): Returns the salary of the employee.
+    get_email_address(): Returns the email address of the employee.
+    """
+
+    def __init__(self, name, ID_number, salary, email_address):
+    # Initializing the private data members
+        self._name = name
+        self._ID_number = ID_number
+        self._salary = salary
+        self._email_address = email_address
+
+    # Define the getter methods
+    def get_name(self):
+        return self._name
+
+    def get_ID_number(self):
+        return self._ID_number
+
+    def get_salary(self):
+        return self._salary
+
+    def get_email_address(self):
+        return self._email_address
+
+# Define employee dictionary list
+def make_employee_dict(names, ids, salaries, emails):
+    """
+    Creates a employee dictionary of objects from the lists of employee information.
+
+    Parameters:
+    names (lists of str): The list of employee names.
+    ids (lists of str): The list of employee ID numbers.
+    salaries (lists of float): The list of employee salaries.
+    emails (list of str): The list of employee email addresses.
+    """
+    # Initialize an empty dictionary to store employee objects
+    employee_dict = {}
+
+    # Use a for loop with zip as the variable to iterate over the lists
+    for name, ID_number, salary, email_address in zip(names, ids, salaries, emails):
+        # Create the employee object for each set of values.
+        employee = Employee(name, ID_number, salary, email_address)
+
+        # Add the Employee object to the dictionary using [ID_number]
+        employee_dict[ID_number] = employee
+
+    # Return the dictionary
+    return employee_dict
+
+# Test data
+names = ["Jean", "Kat", "Pomona"]
+ids = ["100", "101", "102"]
+salaries = [30, 35, 28]
+emails = ["Jean@aol.com", "Kat@aol.com", "Pomona@aol.com"]
+
+# Create the dictionary of employees
+employees = make_employee_dict(names, ids, salaries, emails)
+
+# Print the test of employee emails and names
+print(employees["100"].get_name())
+print(employees["101"].get_name())
+print(employees["102"].get_name())
+
+print(employees["100"].get_email_address())
+print(employees["101"].get_email_address())
